40cbe3a7e2bdc58848e6657dfe6d18834da89406
gxt_kt <gxt_kt@163.com>
2023-10-10 16:34:59 +0800

    add print class attributes define

--- debugstream.hpp
+++ debugstream.hpp
@@ -427,7 +427,7 @@ namespace pprint {
     typename std::enable_if<std::is_enum<T>::value == true, void>::type
     print_internal(T value, size_t indent = 0, const std::string& line_terminator = "\n", size_t level = 0) {
       stream_ << std::string(indent, ' ')
-              << static_cast<std::underlying_type_t<T>>(value)
+              << static_cast<typename std::underlying_type<T>::type>(value)
               << line_terminator;
     }
 
@@ -1099,7 +1099,7 @@ class DebugStream {
   //======================================
   explicit DebugStream \
   (std::function<void(const std::string&)>  fun_ = DebugSendStringCallBack_Default_, \
-  int buf_len_ = 1024);
+  int buf_len_ = 2048);
   DebugStream(const DebugStream &obj);
   DebugStream(DebugStream &&obj);
   DebugStream& operator=(const DebugStream &obj);
@@ -1385,6 +1385,7 @@ inline T PreventNULL(){return value;}
 
 
 // Print split line such as "=============================="
+namespace gxt {
 namespace detail {
 class SplitLine {
  public:
@@ -1428,11 +1429,100 @@ class SplitLine {
   static const int size_ = 30;
   static const char default_char_ = '=';
 };
+}  // namespace detail
+}  // namespace gxt
+
+#define G_SPLIT_LINE gxt::detail::SplitLine()
+
+namespace gxt {
+namespace detail {
+// print class attributes
+template <class T, class D = typename std::enable_if<std::is_class<T>::value, T>::type>
+[[nodiscard]] inline std::string ClassDetail(std::string class_name) {
+  // static_assert(std::is_same<T,D>::value,"T must be class type");
+
+  std::stringstream str;
+  str << std::boolalpha;
+
+#define CLASS_DETAIL_SPLIT(str_) str << G_SPLIT_LINE(str_, 44) << "\n";
+
+#define CLASS_DETAIL_VAR(tt)                                       \
+  str << std::setw(35) << std::left << #tt << "| " << std::setw(5) \
+      << std::tt<T>() << " |\n"
+
+  CLASS_DETAIL_SPLIT(">")
+  str << "class name: " << class_name << "\n";
+  CLASS_DETAIL_SPLIT("=")
+
+  CLASS_DETAIL_VAR(is_constructible);
+  CLASS_DETAIL_VAR(is_trivially_constructible);
+  CLASS_DETAIL_VAR(is_nothrow_constructible);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(is_default_constructible);
+  CLASS_DETAIL_VAR(is_trivially_default_constructible);
+  CLASS_DETAIL_VAR(is_nothrow_default_constructible);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(is_copy_constructible);
+  CLASS_DETAIL_VAR(is_trivially_copy_constructible);
+  CLASS_DETAIL_VAR(is_nothrow_copy_constructible);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(is_move_constructible);
+  CLASS_DETAIL_VAR(is_trivially_move_constructible);
+  CLASS_DETAIL_VAR(is_nothrow_move_constructible);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  // CLASS_DETAIL_VAR(is_assignable);
+  // CLASS_DETAIL_VAR(is_trivially_assignable);
+  // CLASS_DETAIL_VAR(is_nothrow_assignable);
+  //
+  // CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(is_copy_assignable);
+  CLASS_DETAIL_VAR(is_trivially_copy_assignable);
+  CLASS_DETAIL_VAR(is_nothrow_copy_assignable);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(is_move_assignable);
+  CLASS_DETAIL_VAR(is_trivially_move_assignable);
+  CLASS_DETAIL_VAR(is_nothrow_move_assignable);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(is_destructible);
+  CLASS_DETAIL_VAR(is_trivially_destructible);
+  CLASS_DETAIL_VAR(is_nothrow_destructible);
+
+  CLASS_DETAIL_SPLIT("-")
+
+  CLASS_DETAIL_VAR(has_virtual_destructor);
+
+  CLASS_DETAIL_SPLIT("-")
+  // CLASS_DETAIL_VAR(is_swappable_with);
+  CLASS_DETAIL_VAR(is_swappable);
+  // CLASS_DETAIL_VAR(is_nothrow_swappable_with);
+  CLASS_DETAIL_VAR(is_nothrow_swappable);
+
+  CLASS_DETAIL_SPLIT("=")
+  CLASS_DETAIL_SPLIT("<")
+
+#undef CLASS_DETAIL_VAR
+#undef CLASS_DETAIL_SPLIT
+
+  return str.str();
+}
 
 }  // namespace detail
+}  // namespace gxt
 
-#define G_SPLIT_LINE detail::SplitLine()
-
+#define CLASS(type) gxt::detail::ClassDetail<type>(#type)
 
 #include <chrono>
 #include <thread>
