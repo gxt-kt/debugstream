565da4cfabc8671e04d8e5e17e558bbc3238d01b
gxt_kt <gxt_kt@163.com>
2024-12-06 17:58:53 +0800

    add filename with data function

--- example/example_filename.cc
+++ example/example_filename.cc
@@ -1,9 +1,17 @@
 #include "debugstream/debugstream.h"
 
-
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   std::cout << G_GET_FILE_PATH() << std::endl;
   std::cout << G_GET_FILE_FILENAME() << std::endl;
   std::cout << G_GET_FILE_BASENAME() << std::endl;
+  // clang-format off
+  std::cout << gxt::filename::GetFilePathWithData("abc/123.txt") << std::endl;
+  std::cout << gxt::filename::GetFilePathWithData("/abc/123.txt") << std::endl;
+  std::cout << gxt::filename::GetFilePathWithData("/abc/567/123.txt") << std::endl;
+  std::cout << gxt::filename::GetFilePathWithData("123.txt") << std::endl;
+  std::cout << gxt::filename::GetFilePathWithData("123") << std::endl;
+  std::cout << gxt::filename::GetFilePathWithData("/123") << std::endl;
+  std::cout << gxt::filename::GetFilePathWithData("/123/567") << std::endl;
+  // clang-format on
   return 0;
 }
--- include/debugstream/detail/filename.h
+++ include/debugstream/detail/filename.h
@@ -23,5 +23,42 @@ namespace filename {
     return file_name;                                              \
   }()
 
+// 获取当前时间，格式化为 YYYY-MM-DDTHH:MM:SS
+inline std::string GetCurrentTime() {
+  std::time_t now = std::time(nullptr);
+  std::tm *localTime = std::localtime(&now);
+
+  std::ostringstream oss;
+  oss << localTime->tm_year + 1900 << "-" << std::setfill('0') << std::setw(2)
+      << (localTime->tm_mon + 1) << "-" << std::setfill('0') << std::setw(2)
+      << localTime->tm_mday << "_" << std::setfill('0') << std::setw(2)
+      << localTime->tm_hour << ":" << std::setfill('0') << std::setw(2)
+      << localTime->tm_min << ":" << std::setfill('0') << std::setw(2)
+      << localTime->tm_sec;
+
+  return oss.str();
+}
+
+// 创建备份文件路径
+// 例如输入 /abc/123.txt 输出 /abc/123_back_2024-12-06_T_17:50:46.txt
+inline std::string GetFilePathWithData(const std::string &originalPath) {
+  size_t lastSlash = originalPath.find_last_of("/\\");
+  std::string directory = (lastSlash != std::string::npos)
+                              ? originalPath.substr(0, lastSlash + 1)
+                              : "";
+  std::string filename = (lastSlash != std::string::npos)
+                             ? originalPath.substr(lastSlash + 1)
+                             : originalPath;
+
+  size_t dotPos = filename.find_last_of('.');
+  std::string baseName =
+      (dotPos != std::string::npos) ? filename.substr(0, dotPos) : filename;
+  std::string extension =
+      (dotPos != std::string::npos) ? filename.substr(dotPos) : "";
+
+  std::string timeStamp = GetCurrentTime();
+  return directory + baseName + "_" + timeStamp + extension;
+}
+
 }  // namespace filename
 }  // namespace gxt
